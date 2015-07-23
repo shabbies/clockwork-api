69e96561aa4be7fb562d1a1f9bd0f77f57c744a4
Kenneth <kssc91@hotmail.com>
2015-07-23 17:12:22 +0800

    added logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < Devise::SessionsController
 
     def create
     	self.resource = warden.authenticate!(auth_options)
-	    set_flash_message(:notice, :signed_in) if is_flashing_format?
 	    sign_in(resource_name, resource)
 	    yield resource if block_given?
 	    respond_with resource, :location => after_sign_in_path_for(resource) do |format|
@@ -12,4 +11,14 @@ class SessionsController < Devise::SessionsController
 	      	format.json {render :json => resource } # this code will get executed for json request
 	    end
     end
+
+ 	def destroy
+	    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+	    respond_to do |format|
+	        format.json { render :json => { 
+	        	:success => true,
+	        	:message => "user has been successfully signed out"
+        	}}
+	    end
+  	end
 end  
