80a56f590b56fdf6cc27becf58c0a4bd2a0e7df1
Kenneth <kssc91@hotmail.com>
2015-08-27 23:56:52 +0800

    added expiry date

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	nilify_blanks
 
 	belongs_to 	:owner, 		:class_name => "User", 	:foreign_key => "owner_id"
-	has_many	:matchings, 	:dependent => :destroy
+	has_many	:matchings, 	:dependent => :destroy, :foreign_key => "post_id"
 	has_many	:applicants,	:class_name => "User", 	through: :matchings
 end
