ab21dd9a02873c7883d693b2c7bc59faf72507f2
Kenneth <kssc91@hotmail.com>
2015-08-30 12:40:31 +0800

    debug rate

--- app/api/listing.rb
+++ app/api/listing.rb
@@ -218,7 +218,10 @@ class Listing < Grape::API
 			user_feedback_array = JSON.parse params[:user_feedback]
 			user_feedback_array.each do |user_feedback_json|
 				puts user_feedback_json
-				user_feedback = JSON.parse user_feedback_json
+				user_feedback_json.is_a? String
+				user_feedback_json.is_a? Hash
+
+				user_feedback = JSON.parse(user_feedback_json)
 				user_id = user_feedback[:user_id]
 				rating = user_feedback[:rating]
 				comment = user_feedback[:comment]
