19df38f54d6ec82122959a6baf629ae2117657b2
Kenneth <kssc91@hotmail.com>
2015-10-20 13:52:26 +0800

    connected mobile notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,24 +1,24 @@
 class Notification < ActiveRecord::Base
+	before_save :send_mobile_notification
 	belongs_to 	:sender, 		:class_name => "User", 	:foreign_key => "sender_id"
 	belongs_to 	:receiver, 		:class_name => "User", 	:foreign_key => "receiver_id"
 
-	def self.send_mobile_notification(user_id, message)
-		devices = Device.where(owner_id: user_id).all
+	def self.send_mobile_notification
+		devices = Device.where(owner_id: receiver_id).all
 		devices.each do |device|
 			if device.device_type == "ios"
 				n = Rpush::Apns::Notification.new
 				n.app = Rpush::Apns::App.find_by_name("ios_app")
 				n.device_token = "#{device.device_id}"
-				n.alert = message
+				n.alert = content
 				n.save!
 			else
 				n = Rpush::Gcm::Notification.new
 				n.app = Rpush::Gcm::App.find_by_name("android_app")
 				n.registration_ids = "#{device.device_id}"
-				n.data = { message: message }
+				n.data = { message: content }
 				n.save!
 			end
 		end
-		Rpush.push
 	end
 end
