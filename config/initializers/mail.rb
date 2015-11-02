fece18cd172b9d6d647a624ec162e9d6644de3ee
Kenneth <kssc91@hotmail.com>
2015-11-02 18:13:56 +0800

    swapped to app password

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
       :port                 => 587,
       :domain               => "gmail.com",
       :user_name            => "clockworksg.smu@gmail.com",
-      :password             => "Teamclockwork41",
+      :password             => "slnvhhnctjjjicxl",
       :authentication       => :login,
       :enable_starttls_auto => true
 }
