565bf4346c7f9e426f5f475e3082b50414158afe
Kenneth <kssc91@hotmail.com>
2016-02-01 21:14:51 +0800

    edited font awesome cdn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   def create
   	password = Devise.friendly_token.first(8)
 
-  	User.create!(email: user_params[:email], contact_number: user_params[:contact_number], password: password, password_confirmation: password)
+  	unless User.exists?(email: user_params[:email])
+  		User.create!(email: user_params[:email], contact_number: user_params[:contact_number], password: password, password_confirmation: password)
+		end
   	redirect_to "/"
   end
 
