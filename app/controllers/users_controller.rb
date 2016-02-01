9d5f44a8c87c8f0b30ac7cae7260ffb2575e2ea2
Kenneth <kssc91@hotmail.com>
2016-02-01 22:53:41 +0800

    added logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,13 @@ class UsersController < ApplicationController
   def create
   	password = Devise.friendly_token.first(8)
 
-  	unless User.exists?(email: user_params[:email])
-  		User.create!(email: user_params[:email], contact_number: user_params[:contact_number], password: password, password_confirmation: password)
+  	if User.exists?(email: user_params[:email])
+  		flash[:alert] = "Oops! The email is already in use"
+		elsif User.exists?(contact_number: user_params[:contact_number])
+			flash[:alert] = "Oops! The contact number is already in use"
+		else
+			User.create!(email: user_params[:email], contact_number: user_params[:contact_number], password: password, password_confirmation: password)
+  		flash[:notice] = "You have successfully created your account!"
 		end
   	redirect_to "/"
   end
