1992b5287b852b50c692ca03e25426f0a0b9e3d5
Kenneth <kssc91@hotmail.com>
2016-02-02 21:02:41 +0800

    added phone number validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def create
   	password = Devise.friendly_token.first(8)
 
+    unless user_params[:contact_number].is_a? Integer
+      unless user_params[:contact_number] > 60000000 && user_params[:contact_number] < 99999999
+        flash[:alert] = "Oops! Please enter a valid phone number"
+      end
+    end
+
   	if User.exists?(email: user_params[:email])
   		flash[:alert] = "Oops! The email is already in use"
 		elsif User.exists?(contact_number: user_params[:contact_number])
@@ -16,5 +22,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:email, :contact_number)
+  end
+
+  def is_number? string
+    true if Float(string) rescue false
   end
 end
