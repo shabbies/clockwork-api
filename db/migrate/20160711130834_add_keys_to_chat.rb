1f9adcda5d28e10f1054ce76519a30625f8881d8
Kenneth <kssc91@hotmail.com>
2016-07-27 00:29:18 +0800

    edited migration (remove index on post id)

--- db/migrate/20160711130834_add_keys_to_chat.rb
+++ db/migrate/20160711130834_add_keys_to_chat.rb
@@ -1,6 +1,5 @@
 class AddKeysToChat < ActiveRecord::Migration
   def change
-  	add_index :chatrooms, [:post_id], :unique => true
   	add_index :chatroom_participants, [:chatroom_id, :user_id], :unique => true
   end
 end
